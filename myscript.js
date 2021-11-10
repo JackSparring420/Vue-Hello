bcf3a7072585a74420bcddce8d22d6ab164afdd3
JackSparring420 <emanuele.arconte94@gmail.com>
2021-11-10 14:52:47 +0100

    change src vue

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ var app = new Vue(
     {
         el: "#myapp",
         data: {
-            testo: "HELLO vueJS!!",
+            testo: "HELLO VueJS!!",
             immagine: "https://www.codersitalia.it/wp-content/uploads/2020/02/imparare-vuejs.jpeg"
         }
     }
