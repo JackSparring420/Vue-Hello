71f8661274ca8bea697ff7cb07b88d3b595d1150
JackSparring420 <emanuele.arconte94@gmail.com>
2021-11-10 13:23:51 +0100

    exercise completed

---
+++ myscript.js
@@ -0,0 +1,9 @@
+var app = new Vue(
+    {
+        el: "#myapp",
+        data: {
+            testo: "HELLO vueJS!!",
+            immagine: "https://www.codersitalia.it/wp-content/uploads/2020/02/imparare-vuejs.jpeg"
+        }
+    }
+)
